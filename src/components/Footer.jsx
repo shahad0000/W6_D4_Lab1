569cfecd3d51ae99735e32e494752f985520112d
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-05-21 14:07:59 +0300

    improve styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-700 h-[30vh] text-white flex justify-center'>
+    <div className='bg-gray-700 h-[30vh] text-white flex justify-center items-center'>
         copyright: 
     </div>
   )
